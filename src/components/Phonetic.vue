2454b83c35319b239142f17e72184cbb0909371d
Carlos dos Anjos <carlos@goodtravelsoftware.com>
2019-04-11 15:44:34 +0100

    IPA enabled

---
+++ src/components/Phonetic.vue
@@ -0,0 +1,119 @@
+<template>
+  <v-layout align-center>
+
+    <v-flex>
+      <v-window v-model="window" vertical>
+        <v-window-item>
+            <v-card color="transparent" width="100%">
+                <v-layout align-center justify-center fill-height tag="v-card-text">
+                    <div class="name-card">
+                        <h1 class="my-name">
+                            <span>C</span>
+                            <span>A</span>
+                            <span>R</span>
+                            <span>L</span>
+                            <span>O</span>
+                            <span>S</span>
+                            <span>&nbsp;</span>
+                            <span>A</span>
+                            <span>N</span>
+                            <span>J</span>
+                            <span>O</span>
+                            <span>S</span>
+                        </h1>
+                    </div>
+                </v-layout>
+            </v-card>
+        </v-window-item>
+
+         <v-window-item>
+             <v-card color="transparent" width="100%">
+                <v-layout align-center justify-center fill-height tag="v-card-text">
+                    <div class="name-card">
+                        <h1 class="my-name">
+                            <span>/'</span>
+                            <span>k</span>
+                            <span>a:</span>
+                            <span>r</span>
+                            <span>l</span>
+                            <span>ʊ</span>
+                            <span>z/</span>
+
+                            <span>/ɐ̃.</span>
+                            <span>ʒ</span>
+                            <span>ʊ</span>
+                            <span>s/</span>
+                        </h1>
+                    </div>
+                </v-layout>
+             </v-card>
+        </v-window-item>
+
+        <v-item-group v-model="window" mandtory class="shrink text-xs-center">
+            <v-item>
+                <v-btn slot-scope="{ active, toggle }" :input-value="active" icon @click="toggle">
+                    <v-tooltip bottom>
+                        <template v-slot:activator="{ on }">
+                            <span v-on="on" class="language-button">a</span>
+                        </template>
+                        <span>Latin Alphabet</span>
+                    </v-tooltip>
+                </v-btn>
+            </v-item>
+            <v-item>
+                <v-btn slot-scope="{ active, toggle }" :input-value="active" icon @click="toggle">
+                    <v-tooltip bottom>
+                        <template v-slot:activator="{ on }">
+                            <span v-on="on" class="language-button">æ</span>
+                        </template>
+                        <span>IPA</span>
+                    </v-tooltip>
+                </v-btn>
+            </v-item>
+        </v-item-group>
+      </v-window>
+
+    </v-flex>
+  </v-layout>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class Phonetic extends Vue {
+    public window: number = 0;
+}
+</script>
+
+<style lang="scss">
+
+.language-button {
+    text-transform: lowercase;
+}
+
+.my-name {
+    font-size: 4vmax;  
+    font-weight: bold;
+    font-family: Lato, sans-serif;
+    span {
+        opacity: 0.7;
+        font-weight: 800;
+        mix-blend-mode: darken;
+        letter-spacing: 4px;
+        &::selection {
+            background-color: rgb(230, 230, 250);
+        }
+    }
+}
+
+$colours: (#fa1e44, #fec925, #ab47bc, #5ab190, #00b4eb);
+
+@each $colour in $colours {
+  $i: index($colours, $colour);
+
+  .my-name span:nth-child(5n + #{$i}) {
+    color: $colour;
+  }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,108 +1,111 @@
 <template>
-  <v-container text-xs-center>
-    <v-layout align-center column>
-      <header>
-        <h1 class="my-name">
-          <span>C</span>
-          <span>A</span>
-          <span>R</span>
-          <span>L</span>
-          <span>O</span>
-          <span>S</span>
-          <span>&nbsp;</span>
-          <span>A</span>
-          <span>N</span>
-          <span>J</span>
-          <span>O</span>
-          <span>S</span>
-        </h1>
-      </header>
+	<v-container text-xs-center>
+		<v-layout align-center column>
+			<header>
+				<Phonetic></Phonetic>
+			</header>      
 
-      <section class="header">
-        <h2>I love coding and all the stuff around it. My favourite languages are PHP and TypeScript.</h2>
-        <h3>TypeScript is the greatest thing since sliced bread, by the way</h3>
-      </section>
+			<section class="header">
+				<h2>I love coding and all the stuff around it. My favourite languages are PHP and TypeScript.</h2>
+				<h3>TypeScript is the greatest thing since sliced bread, by the way</h3>
+			</section>
 
-      <section class="about-me">
-        <h2>Who am I?</h2>
-        <p>I've been working with computers for most of my professional life. When I was 18 I worked a few months in a catering company. So I can say I know my way around computers and serving drinks.
+			<section class="about-me">
+				<h2>Who am I?</h2>
+				<p>I've been working with computers for most of my professional life. When I was 18 I worked a few months in a catering company. So I can say I know my way around computers and serving drinks.
 
-        Professionally, the first programming language I had contact with was Visual Basic 6, (VB 6 for those who missed it) then I quickly moved to the most loved/hated RDBMS Oracle 9i.
+				Professionally, the first programming language I had contact with was Visual Basic 6, (VB 6 for those who missed it) then I quickly moved to the most loved/hated RDBMS Oracle 9i.
 
-        I had loads of fun with PL/SQL. During this period I was introduced to PHP, then it became my main development tool along with Oracle 9i.
+				I had loads of fun with PL/SQL. During this period I was introduced to PHP, then it became my main development tool along with Oracle 9i.
 
-        In the past 5 years, I've been working with Zend Framework, MariaDB, NativeScript, Angular and a bunch of Azure Cloud related stuff.</p>
-        
-        <h2>What am I up to?</h2>
-        <p>I've been working extensively with JavaScript-related projects, including NativeScript, Node.js, Vue.js and Firebase.</p>
-      </section>
+				In the past 5 years, I've been working with Zend Framework, MariaDB, NativeScript, Angular and a bunch of Azure Cloud related stuff.</p>
+				
+				<h2>What am I up to?</h2>
+				<p>I've been working extensively with JavaScript-related projects, including NativeScript, Node.js, Vue.js and Firebase.</p>
+			</section>
 
-      <v-footer class="justify-center" inset app>
-        <h3>
-          <v-btn flat icon href="https://www.linkedin.com/in/carlosanjos/">
-            <v-icon size="25px">mdi-linkedin</v-icon>
-          </v-btn>
-          <v-btn flat icon href="https://github.com/carlosanjos">
-            <v-icon size="25px">mdi-github-box</v-icon>
-          </v-btn>
-          <v-btn flat icon href="https://twitter.com/carlosanjosdev">
-            <v-icon size="25px">mdi-twitter</v-icon>
-          </v-btn>
-          <v-btn flat icon href="mailto:contact@carlosanjos.dev" target="_blank">
-            <v-icon size="25px">mdi-email</v-icon>
-          </v-btn>
-        </h3>
-      </v-footer>
-    </v-layout>
-  </v-container>
+			<v-footer class="justify-center" inset app>
+				<v-btn color="pink" dark small absolute top right fab>
+					<v-icon>mdi-bell</v-icon>
+				</v-btn>
+				<h3>
+					<v-btn flat icon href="https://www.linkedin.com/in/carlosanjos/">
+						<v-icon size="25px">mdi-linkedin</v-icon>
+					</v-btn>
+					<v-btn flat icon href="https://github.com/carlosanjos">
+						<v-icon size="25px">mdi-github-box</v-icon>
+					</v-btn>
+					<v-btn flat icon href="https://twitter.com/carlosanjosdev">
+						<v-icon size="25px">mdi-twitter</v-icon>
+					</v-btn>
+					<v-btn flat icon href="mailto:contact@carlosanjos.dev" target="_blank">
+						<v-icon size="25px">mdi-email</v-icon>
+					</v-btn>
+				</h3>
+			</v-footer>
+		</v-layout>
+	</v-container>
 </template>
 
 <style lang="scss" scoped>
 .my-name {
-  text-transform: uppercase;
-  font-weight: bold;
-  font-family: Lato, sans-serif;
-  font-size: 4vmax;
-  span {
-    opacity: 0.7;
-    font-weight: 800;
-    mix-blend-mode: darken;
-    letter-spacing: 4px;
-  }
-  &::selection {
-    background-color: rgb(230, 230, 250);
-  }
+	text-transform: uppercase;
+	font-weight: bold;
+	font-family: Lato, sans-serif;
+	font-size: 4vmax;
+	span {
+		opacity: 0.7;
+		font-weight: 800;
+		mix-blend-mode: darken;
+		letter-spacing: 4px;
+	}
+	&::selection {
+		background-color: rgb(230, 230, 250);
+	}
 }
 
 .header {
-  margin: 20px 15px;
-  h3 {
-   margin: 10px 0 0; 
-  }
+	margin: 20px 15px;
+	h3 {
+	 margin: 10px 0 0; 
+	}
 }
 
 .about-me {
-  margin: 20px 15px;
-  h2 {
-    font-size: 22px;
-  }
-  p {
-    text-align: justify;
-    font-size: 16px;
-    line-height: 1.6;
-    word-wrap: break-word;
-    
-  }
+	margin: 20px 15px;
+	h2 {
+		font-size: 22px;
+	}
+	p {
+		text-align: justify;
+		font-size: 16px;
+		line-height: 1.6;
+		word-wrap: break-word;
+		
+	}
 }
 
 // $colours: (red: #fa1e44, yellow: #fec925, seawater: #c9e3db, green : #5ab190, blue: #00b4eb);
 $colours: (#fa1e44, #fec925, #ab47bc, #5ab190, #00b4eb);
 
 @each $colour in $colours {
-  $i: index($colours, $colour);
+	$i: index($colours, $colour);
 
-  .my-name span:nth-child(5n + #{$i}) {
-    color: $colour;
-  }
+	.my-name span:nth-child(5n + #{$i}) {
+		color: $colour;
+	}
 }
 </style>
+<script lang="ts">
+import { Component, Vue, Prop, Provide } from 'vue-property-decorator';
+import Phonetic from '@/components/Phonetic.vue';
+
+@Component({
+	components: {
+		Phonetic,
+	},
+})
+export default class Home extends Vue {
+
+}
+</script>
